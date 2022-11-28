1cbd1f515b63491b0924d19c077c3140e671f9b8
davin <christophorusdavin@gmail.com>
2022-11-28 21:06:43 +0700

    Initial Commit

---
+++ Pods/Target Support Files/Pods-SampleCalculator-SampleCalculatorUITests/Pods-SampleCalculator-SampleCalculatorUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SampleCalculator_SampleCalculatorUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SampleCalculator_SampleCalculatorUITests
+@end
---
+++ SampleCalculator/AppDelegate.swift
@@ -0,0 +1,45 @@
+//
+//  AppDelegate.swift
+//  SampleCalculator
+//
+//  Created by Christophorus Davin on 28/11/22.
+//
+
+import UIKit
+import AppCenter
+import AppCenterCrashes
+import AppCenterAnalytics
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        AppCenter.start(withAppSecret: "777ccdd9-f6de-4962-9257-2e8c2c49a932", services:[
+          Analytics.self,
+          Crashes.self
+        ])
+        
+        
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
